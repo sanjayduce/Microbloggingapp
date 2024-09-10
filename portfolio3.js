58fba5aee04051a0beff219965a166ce6c9e0a39
sanjayduce <sandysanjay004@gmail.com>
2024-09-10 11:02:17 +0530

    Add files via upload

---
+++ portfolio3.js
@@ -0,0 +1,8 @@
+// Example: Smooth scroll for "Back to Top" button
+document.querySelector('.back-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
